d11b50bdeacd10bb0e71c8692688550cb359622a
Nehemia <nehemia.tuerk@gmail.com>
2018-05-16 16:31:43 +0200

    ok

--- app/src/main/java/com/sportfest/grundschul/test1/Home.java
+++ app/src/main/java/com/sportfest/grundschul/test1/Home.java
@@ -3,6 +3,7 @@ package com.sportfest.grundschul.test1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +13,8 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
 
         Button btnWeitsprung, btnStatistik;
         btnWeitsprung = findViewById(R.id.btnWeitsprung);
--- app/src/main/java/com/sportfest/grundschul/test1/LogIn.java
+++ app/src/main/java/com/sportfest/grundschul/test1/LogIn.java
@@ -5,22 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
+
+    private EditText txtUserName;
+    private EditText txtPassword;
+    private TextView txtView1;
+    private Button btnLogIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button btnLogIn;
+        txtUserName = (EditText) findViewById(R.id.txtUsername);
+        txtPassword = (EditText)   findViewById(R.id.txtUsername);
+        txtView1 = (TextView) findViewById(R.id.txtView1);
         btnLogIn = findViewById(R.id.btnLogIn);
         btnLogIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent b = new Intent(getApplicationContext(), Home.class);
-                startActivity(b);
+                validateLogIn(txtUserName.getText().toString(), txtPassword.getText().toString());
             }
         });
+    }
+
+    private void validateLogIn (String txtUserName, String txtPassword){
+        if((txtUserName.equals("Nehemia")) && (txtPassword.equals("1234"))){
+            Intent b = new Intent(getApplicationContext(), Home.class);
+            startActivity(b);
+        }
+        else{
+            txtView1.setText("Username oder Password falsch");
+        }
     }
 }
